19d31a782cbad8ae0f83b28ec9bb36d0a5c0818e
김건우 <kw.kim@xenoimpact.com>
2021-06-19 21:09:18 +0900

    [Feature] - Created new Class, named Body

---
+++ lib/Lib/Screens/Main/Body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black54,
+      child: Center(
+        child: Text("Hello Flutter")
+      )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        routes: {
+          "/": (context) => Body(),
+          "/detail": (context) => Body(),
+          "/test": (context) => Body()
+        },
+        home: Home(),
+        actions: {});
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Testing.."),
+        actions: [],
+      ),
+      body: Body(),
+      backgroundColor: Colors.amber,
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
